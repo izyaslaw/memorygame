f488c1d1f99f30a11da2c84ff7d78511c1aecc93
izyaslaw <izyaslaw1488@gmail.com>
2017-12-17 22:20:06 +0500

    qwe
    qwe

--- scripts/createCardPlace.js
+++ scripts/createCardPlace.js
@@ -1,7 +1,8 @@
 for (i=1; i<=3; i++) {
     var gameField = $("#game-field");
-    $("body").append("<div>")
+    gameField.append('<div class="cards-line" id="line-' + i + '">');
+    var line = $("#line-" + i)
     for (j=1; j<=6; j++) {
-
+        line.append('<div class="card" id="' + i + '-' + j + '" data-tid="Card">')
     }
 }
